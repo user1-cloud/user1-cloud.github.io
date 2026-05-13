6abd18760d6ab9e64f727c3336e84ec3db9fa8af
user1-cloud <easbeasbeasb@outlook.com>
2026-05-13 14:30:34 +0800

    优化样式

--- astro.config.mjs
+++ astro.config.mjs
@@ -30,18 +30,7 @@ export default defineConfig({
     compress(),
   ],
 
-  fonts: [{
-    provider: fontProviders.local(),
-    name: 'Atkinson',
-    cssVariable: '--font-atkinson',
-    fallbacks: ['sans-serif'],
-    options: {
-      variants: [
-        { src: ['./src/assets/fonts/atkinson-regular.woff'], weight: 400, style: 'normal' },
-        { src: ['./src/assets/fonts/atkinson-bold.woff'], weight: 700, style: 'normal' },
-      ],
-    },
-  }],
+  fonts: [],
 
   markdown: {
     remarkPlugins: [
